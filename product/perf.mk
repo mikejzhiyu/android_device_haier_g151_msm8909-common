db60b29211edb794f02c702a45ee3439029382f4
fzkangga <titahpriyangga@gmail.com>
2019-08-07 11:44:50 +0700

    update

--- product/perf.mk
+++ product/perf.mk
@@ -1,3 +1,7 @@
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.am.reschedule_service=true
+    ro.sys.fw.dex2oat_thread_count=4
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.am.reschedule_service=true \
+    ro.config.max_starting_bg=8
