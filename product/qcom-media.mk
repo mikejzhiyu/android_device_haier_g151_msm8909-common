a9eb34910194f270c5f894319bd52a8479d311a7
fzkangga <titahpriyangga@gmail.com>
2019-08-10 07:03:26 +0700

    remove

--- product/qcom-media.mk
+++ product/qcom-media.mk
@@ -7,8 +7,7 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
 
 # Storage
 PRODUCT_PROPERTY_OVERRIDES += \
