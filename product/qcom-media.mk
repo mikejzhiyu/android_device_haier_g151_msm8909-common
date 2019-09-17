a2d399fa76dbec6362ac4913f463d9b57c427726
fzkangga <titahpriyangga@gmail.com>
2019-09-17 11:14:49 +0700

    msm8909: move and update prop

--- product/qcom-media.mk
+++ product/qcom-media.mk
@@ -7,7 +7,3 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
-
-# Storage
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sys.sdcardfs=true
--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -8,13 +8,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
-# Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.data.qmi.adb_logmask=0 \
-    persist.radio.apm_sim_not_pwdn=1 \
-    ro.telephony.call_ring.multiple=false \
-    ro.use_data_netmgrd=true
-
 # RIL
 PRODUCT_PACKAGES += \
     libcnefeatureconfig \
