ec7107b97748332c2e3b1845bd30e80fdccd2ba5
fzkangga <titahpriyangga@gmail.com>
2019-12-03 15:57:15 +0700

    g151: remove power

--- msm8909.mk
+++ msm8909.mk
@@ -149,10 +149,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
-
 # Radio
 PRODUCT_PACKAGES += \
     libcnefeatureconfig \
