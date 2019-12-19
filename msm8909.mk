54d1a84f036adb04942da50f6e2603dab0df86c9
fzkangga <titahpriyangga@gmail.com>
2019-12-19 13:05:04 +0700

    msm8909: Use Android Go configurations

--- msm8909.mk
+++ msm8909.mk
@@ -15,6 +15,9 @@
 # limitations under the License.
 #
 
+# Common Android Go configurations
+$(call inherit-product, build/target/product/go_defaults.mk)
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_ENFORCE_RRO_TARGETS := framework-res
